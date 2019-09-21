3a8633cda74459ac27442bca3bd28ee5d689438f
Mostasim <mostasim.10@gmail.com>
2019-09-21 20:03:06 +0600

    phone auth added

--- app/src/main/java/tech/mbsoft/otpnumberfocusui/MainActivity.java
+++ app/src/main/java/tech/mbsoft/otpnumberfocusui/MainActivity.java
@@ -1,23 +1,53 @@
 package tech.mbsoft.otpnumberfocusui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity implements TextWatcher, View.OnKeyListener {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int RC_SIGN_IN = 123;
+
     EditText et_otp_1, et_otp_2, et_otp_3, et_otp_4;
+    Button btnPhoneAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initUI();
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+
+        btnPhoneAuth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(
+                        AuthUI.getInstance()
+                                .createSignInIntentBuilder()
+                                .setAvailableProviders(Arrays.asList(
+                                        new AuthUI.IdpConfig.PhoneBuilder().build()
+                                        ))
+                                .build(),
+                        RC_SIGN_IN);
+            }
+        });
+
     }
 
     private void initUI() {
@@ -25,6 +55,8 @@ public class MainActivity extends AppCompatActivity implements TextWatcher, View
         et_otp_2 = findViewById(R.id.et_otp_2);
         et_otp_3 = findViewById(R.id.et_otp_3);
         et_otp_4 = findViewById(R.id.et_otp_4);
+
+        btnPhoneAuth = findViewById(R.id.btnPhoneAuth);
 
         et_otp_1.addTextChangedListener(this);
         et_otp_2.addTextChangedListener(this);
@@ -100,5 +132,11 @@ public class MainActivity extends AppCompatActivity implements TextWatcher, View
                 break;
         }
         return false;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.e(TAG,"code->"+requestCode);
     }
 }
